1c2cd65ae135e0102a6a8a0a16eb0f76631977d7
caohaisil201 <caohaisil@gmail.com>
2021-11-15 14:24:05 +0700

    add table to admin server

--- Admin/routes/products.js
+++ Admin/routes/products.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', body:'dashboard' });
+  res.render('index', { title: 'Express', body:'products' });
 });
 
 module.exports = router;
