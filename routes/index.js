b892f7cb5816a8c2fd92aa1d508ab70e04e2f2a8
Duc Ngo <trongduc1509@gmail.com>
2021-11-20 00:09:59 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '' ,body: 'home'});
+  res.render('index', { title: '', header: 'default/header', body: 'home', footer: 'default/footer'});
 });
 
 module.exports = router;
