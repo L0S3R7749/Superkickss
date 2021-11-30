4c9294530cd6d2a18f9d5326fd5f6cc23e19bef4
Duc Ngo <trongduc1509@gmail.com>
2021-11-30 16:13:45 +0700

    Finish Searching & Pagination

--- component/mainRouter.js
+++ component/mainRouter.js
@@ -2,7 +2,6 @@ const indexRouter = require("./homepage");
 const aboutRouter = require("./about");
 const contactRouter = require("./contact");
 const shopRouter = require("./product");
-const productDetail = require("./product/indexDetail");
 const express = require('express');
 const router = express.Router();
 
@@ -11,6 +10,5 @@ router.use("/", indexRouter);
 router.use("/about",aboutRouter);
 router.use("/contact",contactRouter);
 router.use("/product", shopRouter);
-router.use("/product-detail", productDetail);
 
 module.exports = router;
--- component/product/productsController.js
+++ component/product/productsController.js
@@ -4,15 +4,36 @@ const apicaller = require('../../public/js/apiCaller');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    apicaller.callApi(`product/api?page=${req.query.page}`,'GET',null)
+    apicaller.callApi(`product/paramsApi?page=${req.query.page}`,'GET',null)
         .then(function(responseData) {
-            res.render('./default/index', {title: 'Products', body: '../product/shop', products: responseData.data.products, current: responseData.data.current, pages: responseData.data.pages});
+            res.render('./default/index', {title: 'Products', body: '../product/shop', home: '/product?', products: responseData.data.products, current: responseData.data.current, pages: responseData.data.pages});
         })
         .catch(err => {
             res.send(err);
+        });
+});
+
+router.get('/detail', function(req, res, next) {
+    apicaller.callApi(`product/api?id=${req.query.id}`,'GET',null)
+      .then(function(responseData) {
+        res.render('./default/index', { title: 'Product Detail' ,body: '../product/detail', product: responseData.data});
+      })
+      .catch(err => {
+        res.send(err);
+      });
+});
+
+router.get('/search', function(req, res, next) {
+    apicaller.callApi(`product/paramsApi?search=${req.query.search}&page=${req.query.page}`,'GET',null)
+        .then(function(responseData) {
+            res.render('./default/index', {title: 'Products', body: '../product/shop', home: `/product/search?search=${req.query.search}&`, products: responseData.data.products, current: responseData.data.current, pages: responseData.data.pages});
         })
+        .catch(err => {
+            res.send(err);
+        });
 });
 
 router.get('/api', services.find);
+router.get('/paramsApi', services.list);
 
 module.exports = router;
