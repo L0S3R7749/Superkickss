4f51d0f0a9743eb386781ce6fe98eb626cd21890
Duc Ngo <trongduc1509@gmail.com>
2021-12-04 20:32:03 +0700

    Link to info page

--- component/auth/index.js
+++ component/auth/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const loginController = require('./login');
 const signupController = require('./signup');
 const forgotPwController = require('./forgotpassword');
+const infoController = require('./info');
 
 router.use("/login", loginController);
 router.use("/signup", signupController);
 router.use("/forgotpw", forgotPwController);
+router.use("/info", infoController);
 
 module.exports=router;
