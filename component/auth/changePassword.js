d77d9277f11204f5ef6c61b885c74c35e99b213d
caohaisil201 <caohaisil@gmail.com>
2021-12-05 11:32:37 +0700

    add userinfo and change password view

--- component/auth/changePassword.js
+++ component/auth/changePassword.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('./default/index',{title: 'User info',body: '../auth/info'})
+    res.render('./default/index',{title: 'Change password',body: '../auth/changepassword'})
 })
 
 module.exports = router;
--- component/auth/index.js
+++ component/auth/index.js
@@ -4,11 +4,13 @@ const loginController = require('./login');
 const signupController = require('./signup');
 const forgotPwController = require('./forgotpassword');
 const infoController = require('./info');
+const changePasswordController = require('./changePassword');
 
 router.use("/login", loginController);
 router.use("/signup", signupController);
 router.use("/forgotpw", forgotPwController);
 router.use("/info", infoController);
+router.use("/changepw",changePasswordController)
 
 router.get('/logout', function(req,res) {
     req.logOut();
