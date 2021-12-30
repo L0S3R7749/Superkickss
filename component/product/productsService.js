dfccf5f93bbb1498a5583c772f17e7086ca793af
caohaisil201 <caohaisil@gmail.com>
2021-12-30 13:14:04 +0700

    filter product

--- component/product/productsService.js
+++ component/product/productsService.js
@@ -1,25 +1,115 @@
 const Product = require('../../models/Product');
 
 module.exports = {
-    list: (page=1,perPage=9) => {
+    list: (page = 1, perPage = 9) => {
         return Product
             .find()
             .skip((perPage * page) - perPage)
             .limit(perPage);
     },
 
-    search_list: (searchString,page=1,perPage=9) => {
+    search_list: (searchString, page = 1, perPage = 9) => {
         let myquery = {};
-        myquery = {"name" : {$regex : searchString, $options : 'i'}};
+        myquery = {
+            "name": {
+                $regex: searchString,
+                $options: 'i'
+            }
+        };
         return Product
             .find(myquery)
             .skip((perPage * page) - perPage)
             .limit(perPage);
     },
 
+    filter_list: (gender, type, min, max, page = 1, perPage = 9) => {
+        if (gender == '' && type == '') {
+            return Product.find({
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+                .skip((perPage * page) - perPage)
+                .limit(perPage);
+        } else if (gender == '') {
+            return Product.find({
+                    'category.type': type,
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+                .skip((perPage * page) - perPage)
+                .limit(perPage);
+        } else if (type == '') {
+            return Product.find({
+                    'category.gender': gender,
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+                .skip((perPage * page) - perPage)
+                .limit(perPage);
+        } else {
+            return Product.find({
+                    'category.gender': gender,
+                    'category.type': type,
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+                .skip((perPage * page) - perPage)
+                .limit(perPage);
+        }
+    },
+
     customCount: (searchString) => {
         return Product
-            .countDocuments(searchString ? {"name" : {$regex : searchString, $options : 'i'}} : {});
+            .countDocuments(searchString ? {
+                "name": {
+                    $regex: searchString,
+                    $options: 'i'
+                }
+            } : {});
+    },
+
+    filterCount: (gender,type,min,max)=>{
+        if (gender == '' && type == '') {
+            return Product.countDocuments({
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+        } else if (gender == '') {
+            return Product.countDocuments({
+                    'category.type': type,
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+        } else if (type == '') {
+            return Product.countDocuments({
+                    'category.gender': gender,
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+        } else {
+            return Product.countDocuments({
+                    'category.gender': gender,
+                    'category.type': type,
+                    price: {
+                        $gt: min,
+                        $lt: max
+                    }
+                })
+        }
     },
 
     findSingleProduct: (id) => {
@@ -27,9 +117,9 @@ module.exports = {
             .findById(id);
     },
 
-    rating: async (productId,userId,rating,content) =>  {
-        let product=await Product.findById(productId);
-        let comment={
+    rating: async (productId, userId, rating, content) => {
+        let product = await Product.findById(productId);
+        let comment = {
             userId: userId,
             rating: rating,
             content: content,
@@ -39,12 +129,12 @@ module.exports = {
         return comment;
     },
 
-    getRating: async (productId,page,perPage=5) =>{
-        let product=await Product.findById(productId)
-                                .populate('comments.userId');
+    getRating: async (productId, page, perPage = 5) => {
+        let product = await Product.findById(productId)
+            .populate('comments.userId');
 
-        let comments=product.comments.slice(perPage*page-perPage,perPage*page);
-        const respone={
+        let comments = product.comments.slice(perPage * page - perPage, perPage * page);
+        const respone = {
             allComment: product.comments,
             comments,
         };
