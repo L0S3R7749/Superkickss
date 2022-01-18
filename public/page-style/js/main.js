d99ad7f3c7f66d55ba8657fb5a74cfc6f0f296c1
Duc Ngo <trongduc1509@gmail.com>
2022-01-18 17:32:30 +0700

    Fix price range

--- public/page-style/js/main.js
+++ public/page-style/js/main.js
@@ -152,7 +152,7 @@ jQuery(document).ready(function($) {
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 150,
+      max: 1000,
       values: [ 0, 1000 ],
       slide: function( event, ui ) {
         $( "#price" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
